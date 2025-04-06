b3c9e910a76068bbd239a54b7d72208c6ec0a980
nitesh-kumar007 <niteshpunjabi094@gmail.com>
2025-04-06 21:44:34 +0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
 import pages from "./utils/pages";
 import Layout from "./components/layout/Layout";
 import Home from "./components/pages/Home/index";
 import Bookings from "./components/pages/Bookings";
 import ConfirmedBooking from "./components/pages/Bookings/ConfirmedBooking";
-import NotFound from "./components/pages/NotFound/index";
 import UnderConstruction from "./components/pages/UnderConstruction/index";
 import About from "./components/pages/About/About";
 import LoginPage from "./components/pages/Login_Registration/LogInPage";
 
 const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const validPaths = Array.from(pages.values()).map((p) => p.path);
+    const isValidPath = validPaths.includes(location.pathname);
+
+    if (!isValidPath && location.pathname !== "/") {
+      toast.error("Oops! Page not found. Redirecting to Home...", {
+        duration: 2000,
+      });
+      setTimeout(() => {
+        navigate(pages.get("home").path);
+      }, 2000);
+    }
+  }, [location, navigate]);
+
   return (
     <>
+      <Toaster />
       <Layout>
         <Routes>
           <Route path={pages.get("home").path} element={<Home />} />
@@ -30,7 +50,7 @@ const App = () => {
             element={<UnderConstruction />}
           />
           <Route path={pages.get("login").path} element={<LoginPage />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path='*' element={<></>} /> {/* Handled by useEffect */}
         </Routes>
       </Layout>
     </>
