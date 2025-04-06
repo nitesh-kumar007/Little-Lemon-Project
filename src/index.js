84240486614ddd376834a11f0615d19d6625a507
nitesh-kumar007 <niteshpunjabi094@gmail.com>
2025-04-06 22:57:46 +0500

    Switched To HashRouter For Git PAges Fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import 'modern-normalize/modern-normalize.css';
-import './index.css';
-import App from './App';
-import ScrollToTop from './components/layout/ScrollToTop';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "modern-normalize/modern-normalize.css";
+import "./index.css";
+import App from "./App";
+import ScrollToTop from "./components/layout/ScrollToTop";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
